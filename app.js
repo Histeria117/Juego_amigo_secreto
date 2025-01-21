5a110a4dd6a55c9c993c22146d6b8ea7f6eb5fbd
Angel Castillo <angelo-terriblee@hotmail.com>
2025-01-21 15:03:59 -0600

    Funcion, argegar amigos actualizada

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ let secret_friend=[];
 //secret friend
 
 function agregarAmigo(){
+    
   let friend_user=document.getElementById(`amigo`).value;
-  secret_friend.push(friend_user);
+  if(verificar_repetidos(friend_user)){
+      secret_friend.push(friend_user);
+      clear_box();
+  }else{
+    clear_box();
+    alert(`El amigo ${friend_user} ya ah sido agregado a la lista`);
+    
+  }
+  console.log(secret_friend);
 }
 
+function verificar_repetidos(A){
+    for(let i=0;i<=secret_friend.length;i++){
+        if(A==secret_friend[i]){
+            return false
+        }
+    }
+    return true;
+}
+
+function clear_box(){
+    document.querySelector("#amigo").value=``;
+}
 
 
